915b0502b8317859a55a2238fa7fb3444abc7299
GitBashing <62035401+GitBashing@users.noreply.github.com>
2020-03-17 14:54:22 +0000

    Add files via upload

---
+++ news.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#I find it easiest to use this script if it is saved in '/home/yourname/' , then all that you should be required to do is run './news.sh' in terminal.
+#news: general, political, social ,sport, tech, FOSS
+
+firefox https://bbc.co.uk/ https://opendemocracy.net/ https://aljazeera.com/ https://uk.reuters.com/ https://newint.org/ https://apnews.com/ https://thebristolcable.org/ https://www.wokingfc.co.uk/ https://itsfoss.com/ https://fossbytes.com https://www.reddit.com/r/ukpolitics/ https://fullfact.org/
